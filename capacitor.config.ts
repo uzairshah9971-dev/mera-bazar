f75e1980315891b871cdc2f407f5663b472fa2e5
uzairshah9971-dev <uzairshah9971@gmail.com>
2026-09-22 18:03:56 +0500

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uzairshah9971.merabazaar',
+  appName: 'Mera Bazaar',
+  webDir: 'dist',
+};
+
+export default config;
